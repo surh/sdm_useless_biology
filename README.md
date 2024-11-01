b9f74c312a55d932b2293cc463e03d293c7957dc
sur <surh@users.noreply.github.com>
2024-11-01 15:39:40 -0600

    Updating requirements

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # sdm_useless_biology
 
-
 Based on the code from:
 
     Aguirre-Liguori, Jonás A., Santiago Ramírez-Barahona, and Brandon S. Gaut. 
     “The Evolutionary Genomics of Species’ Responses to Climate Change.” 
     Nature Ecology & Evolution 5, no. 10 (October 2021): 1350–60.
     https://doi.org/10.1038/s41559-021-01526-9.
+
+
+# Requirements
+
+- R packages
+  * tidyverse
+  * geodata
+  * terra
+  * sp
+  * biomod2
--- teosintle_get_bioclim.r
+++ teosintle_get_bioclim.r
@@ -73,12 +73,13 @@ sessionInfo()
 
 #' List all  of models, pathways, and time periods to
 #' download forecast data
-model <- c("ACCESS-CM2", "ACCESS-ESM1-5", "AWI-CM-1-1-MR", "BCC-CSM2-MR", 
-  "CanESM5", "CanESM5-CanOE", "CMCC-ESM2", "CNRM-CM6-1", "CNRM-CM6-1-HR",
-  "CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3-Veg-LR", "FIO-ESM-2-0",
-  "GFDL-ESM4", "GISS-E2-1-G", "GISS-E2-1-H", "HadGEM3-GC31-LL", "INM-CM4-8",
-  "INM-CM5-0", "IPSL-CM6A-LR", "MIROC-ES2L", "MIROC6", "MPI-ESM1-2-HR",
-  "MPI-ESM1-2-LR", "MRI-ESM2-0", "UKESM1-0-LL")
+model <- c("ACCESS-CM2", "ACCESS-ESM1-5", "AWI-CM-1-1-MR", "BCC-CSM2-MR",
+           "CanESM5", "CanESM5-CanOE", "CMCC-ESM2", "CNRM-CM6-1",
+           "CNRM-CM6-1-HR", "CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3-Veg-LR",
+           "FIO-ESM-2-0", "GFDL-ESM4", "GISS-E2-1-G", "GISS-E2-1-H",
+           "HadGEM3-GC31-LL", "INM-CM4-8", "INM-CM5-0", "IPSL-CM6A-LR",
+           "MIROC-ES2L", "MIROC6", "MPI-ESM1-2-HR", "MPI-ESM1-2-LR",
+           "MRI-ESM2-0", "UKESM1-0-LL")
 ssp <- c("126", "245", "370", "585")
 time <- c("2041-2060", "2061-2080", "2081-2100")
 
