ce78b38000e841d1b62ccf2c2f0cc36a9a395f1d
sur <surh@users.noreply.github.com>
2024-11-01 13:50:24 -0600

    improve documentation

--- teosintle_get_bioclim.r
+++ teosintle_get_bioclim.r
@@ -16,7 +16,7 @@ world_map <- geodata::world(resolution = 3,
                             path = "data/")
 
 #' Find quadrant that eoncompasses species distribution, increase each
-#' size 1.5 fold. Save data to raster object and save objects for future use.
+#' side of the quadrant 1.5 fold. 
 max_lat <- ceiling(max(Obs$latitude))
 min_lat <- floor(min(Obs$latitude))
 max_lon <- ceiling(max(Obs$longitude))
@@ -24,26 +24,36 @@ min_lon <- floor(min(Obs$longitude))
 quadrant <- terra::ext(x = c(min_lon, max_lon, min_lat, max_lat))
 quadrant <- quadrant * 1.5
 
+#' Crop bioclimatic and map data to the quadrant
 quadrant_map <- terra::crop(x = world_map, y = quadrant)
 bioclim_data <- terra::crop(x = bioclim_data, y = quadrant)
 
-terra::writeRaster(bioclim_data, filename = "data/teosintle_bioclim_raster.tif")
+#' Saved cropped data for future use. These files are the input
+#' for analysis in the class.
+terra::writeRaster(bioclim_data, filename = "data/teosintle_bioclim_raster2.tif")
 terra::writeVector(quadrant_map, filename = "data/teosintle_map")
 
 
 #' # Scenario data
-#' Download proyected bioclimatic variables under different climate scenarios
-#` from the Coupled Model Intercomparison Project (CMIP) 6
+#' Download proyected bioclimatic variables frin different models under
+#' different Shared Socioeconomic Pathways (SSP) from the Coupled Model
+#' Intercomparison Project (CMIP) 6.
+#' Takes a long time to download (100s of MB per model and pathway combination)
 forecast_data <- geodata::cmip6_world(model = "MPI-ESM1-2-HR",
                                       ssp = "245",
                                       time = "2081-2100",
                                       var = "bioc",
                                       res = 2.5,
                                       path = "data")
+
+#' Crop forecast data to the quadrant and save for future use                                    
 forecast_data <- terra::crop(forecast_data, quadrant)
 names(forecast_data) <- names(bioclim_data)
 terra::writeRaster(forecast_data,
   filename = "data/teosintle_forecast_2081-2100_raster.tif")
+
+
+
 
 
 forecast_data <- geodata::cmip6_world(model = "MPI-ESM1-2-HR",
