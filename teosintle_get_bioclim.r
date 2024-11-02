6163e39c7b23e46d1dcfc128a8a23b8c2bbebe7b
surh <sur00mx@gmail.com>
2024-11-02 13:44:34 -0600

    getting forecast data

--- teosintle_get_bioclim.r
+++ teosintle_get_bioclim.r
@@ -30,7 +30,8 @@ bioclim_data <- terra::crop(x = bioclim_data, y = quadrant)
 
 #' Saved cropped data for future use. These files are the input
 #' for analysis in the class.
-terra::writeRaster(bioclim_data, filename = "data/teosintle_bioclim_raster2.tif")
+terra::writeRaster(bioclim_data,
+                   filename = "data/teosintle_bioclim_raster2.tif")
 terra::writeVector(quadrant_map, filename = "data/teosintle_map")
 
 
@@ -38,41 +39,9 @@ terra::writeVector(quadrant_map, filename = "data/teosintle_map")
 #' Download proyected bioclimatic variables frin different models under
 #' different Shared Socioeconomic Pathways (SSP) from the Coupled Model
 #' Intercomparison Project (CMIP) 6.
-#' Takes a long time to download (100s of MB per model and pathway combination)
-forecast_data <- geodata::cmip6_world(model = "MPI-ESM1-2-HR",
-                                      ssp = "245",
-                                      time = "2081-2100",
-                                      var = "bioc",
-                                      res = 2.5,
-                                      path = "data")
-
-#' Crop forecast data to the quadrant and save for future use                                    
-forecast_data <- terra::crop(forecast_data, quadrant)
-names(forecast_data) <- names(bioclim_data)
-terra::writeRaster(forecast_data,
-                    filename = "data/teosintle_forecast_2081-2100_raster.tif")
-
-
-
-
-
-forecast_data <- geodata::cmip6_world(model = "MPI-ESM1-2-HR",
-                                      ssp = "245",
-                                      time = "2061-2080",
-                                      var = "bioc",
-                                      res = 2.5,
-                                      path = "data")
-forecast_data <- terra::crop(forecast_data, quadrant)
-names(forecast_data) <- names(bioclim_data)
-terra::writeRaster(forecast_data,
-                    filename = "data/teosintle_forecast_2061-2080_raster.tif")
-
-
-sessionInfo()
-
 
 #' List all  of models, pathways, and time periods to
-#' download forecast data
+#' download forecast data (some combinations don't exist).
 model <- c("ACCESS-CM2", "ACCESS-ESM1-5", "AWI-CM-1-1-MR", "BCC-CSM2-MR",
            "CanESM5", "CanESM5-CanOE", "CMCC-ESM2", "CNRM-CM6-1",
            "CNRM-CM6-1-HR", "CNRM-ESM2-1", "EC-Earth3-Veg", "EC-Earth3-Veg-LR",
@@ -81,13 +50,11 @@ model <- c("ACCESS-CM2", "ACCESS-ESM1-5", "AWI-CM-1-1-MR", "BCC-CSM2-MR",
            "MIROC-ES2L", "MIROC6", "MPI-ESM1-2-HR", "MPI-ESM1-2-LR",
            "MRI-ESM2-0", "UKESM1-0-LL")
 ssp <- c("126", "245", "370", "585")
-time <- c("2041-2060", "2061-2080", "2081-2100")
-
-
+time <- c("2041-2060", "2061-2080", "2081-2100") # Ignoring period 2021-2040
 
 #' Download all forecast data, for all combinations of models, pathways and
 #' timeframes. Crop each forecast the species quadrant, and save for future use.
-#' This will take a long time to run (100s of MB per combination)
+#' This will take a long time to run (~440 MB per combination, 129 GB total)
 expand.grid(model = model, ssp = ssp, time = time) %>%
   mutate(path = paste0("data/teosintle_forecast_",
                        time, "_", ssp, "_", model, ".tif")) %>%
@@ -97,45 +64,11 @@ expand.grid(model = model, ssp = ssp, time = time) %>%
                                               time = time,
                                               var = "bioc",
                                               res = 2.5,
-                                              path = "data")))  %>%
+                                              path = "data"))) %>%
+  filter(!is.null(forecast)) %>% # Some combinations don't exist
   mutate(forecast = list(terra::crop(forecast, quadrant))) %>%
-  mutate(forecast = list(terra::writeRaster(forecast, filename = path))) %>%
-  ungroup()
+  mutate(forecast = list(terra::writeRaster(forecast, filename = path)))
 
+#' Final size is about 162 MB
 
-
-
-
-
-
-
-forecasts %>% 
-  mutate(path = paste0("data/teosintle_forecast_",
-         time, "_", ssp, "_", model, ".tif")) %>%
-  mutate(forecast = map2(model, ssp, ~geodata::cmip6_world(model = .x,
-                                                            ssp = .y,
-                                                            time = time,
-                                                            var = "bioc",
-                                                            res = 2.5,
-                                                            path = paste0("data/teosintle_forecast_", time, "_", ssp, "_", .x, ".tif")))) %>%
-  mutate(forecast = map2(forecast, path, ~{
-    terra::writeRaster(.x, filename = .y)
-    .x
-  })) %>%
-  select(-path) %>%
-  write_rds("data/teosintle_forecasts.rds")
-
-
-
-
-forecast_data <- geodata::cmip6_world(model = NULL,
-                                      ssp = NULL,
-                                      time = "2081-2100",
-                                      var = "bioc",
-                                      res = 2.5,
-                                      path = "data")
-
-#' Crop forecast data to the quadrant and save for future use                                    
-forecast_data <- terra::crop(forecast_data, quadrant)
-names(forecast_data) <- names(bioclim_data)
-
+sessionInfo()
