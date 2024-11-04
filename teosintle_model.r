c7843802e02c0ada7cbec86447f50c260a893887
surh <sur00mx@gmail.com>
2024-11-03 19:57:27 -0600

    run biomod2 max entropy model

--- teosintle_model.r
+++ teosintle_model.r
@@ -169,80 +169,86 @@ par(op)
 #             parallel = TRUE,
 #             numCores = 8)
 
-
+#' Looad library
 library(tidyverse)
 library(biomod2)
 
+#' Read observation data, current and forecasted bioclimatic data
 Dat <- read_csv("data/teosintle_maxent_input.csv")
 bioclim <- terra::rast("data/teosintle_bioclim_raster.tif")
 bioclim6180 <- terra::rast("data/teosintle_forecast_2061-2080_245_CMCC-ESM2.tif")
-names(bioclim6180) <- names(bioclim)
-
+names(bioclim6180) <- names(bioclim) # Make sure the names are the same!!
 quadrant_map <- terra::vect("data/teosintle_map/")
 
+#' # Using the biomod2 package
 
+#' Prepare data
 g1.bmd <- BIOMOD_FormatingData(
   resp.var = ifelse(Dat$ID == "g1", 1, NA),
   expl.var = bioclim,
   resp.xy = Dat[, c("lon", "lat")],
-  resp.name = "g1", PA.nb.rep = 4,
+  resp.name = "g1", 
+  PA.nb.rep = 4,
   PA.nb.absences = 1000,
   PA.strategy = "random",
   filter.raster = TRUE
 )
 
+#' Run models
+g1.bm <- BIOMOD_Modeling(
+  bm.format = g1.bmd,
+  modeling.id = "g1.bm",
+  models = c("MAXNET"),
+  CV.strategy = "random",
+  CV.nb.rep = 2,
+  CV.perc = 0.8,
+  metric.eval = c("ROC", "TSS"),
+#  OPT.strategy = "tuned", # Normally you should tune the models
+  var.import = 3
+)
 
-g1.bm <- BIOMOD_Modeling(bm.format = g1.bmd,
-                         modeling.id = "g1.bm",
-                         models = c("MAXNET"),
-                         CV.strategy = 'random',
-                         CV.nb.rep = 2,
-                         CV.perc = 0.8,
-                         metric.eval = c("ROC", "TSS"),
-                         var.import = 3)
+#' Project models
+g1.bmp <- BIOMOD_Projection(
+  bm.mod = g1.bm,
+  proj.name = "Current",
+  new.env = bioclim6180,
+  models.chosen = "all",
+  metric.binary = "all",
+  metric.filter = "all",
+  build.clamping.mask = TRUE
+)
+
+#' # Explore results with biomod2 functions
+
+# # Get evaluation scores & variables importance
+# get_evaluations(g1.bm)
+# get_variables_importance(g1.bm) %>% 
+#   as_tibble() %>%
+#   filter(run != "allRun") %>%
+#   ggplot(aes(x = expl.var, y = var.imp, col = run)) +
+#   facet_wrap(~ expl.var, scales = "free_x") +
+#   geom_boxplot(outlier.colour = NA) +
+#   geom_point(position = position_jitterdodge()) +
+#   theme_classic() +
+#   theme(axis.text.x = element_blank())
+
+# # Represent evaluation scores & variables importance
+# get_calib_lines(g1.bm) %>% plot(g1.bmd, calib.lines = .)
+# bm_PlotEvalMean(bm.out = g1.bm)
+# bm_PlotEvalBoxplot(bm.out = g1.bm, group.by = c('algo', 'algo'))
+# bm_PlotEvalBoxplot(bm.out = g1.bm, group.by = c('algo', 'run'))
+# bm_PlotVarImpBoxplot(bm.out = g1.bm, group.by = c('expl.var', 'algo', 'algo'))
+# bm_PlotVarImpBoxplot(bm.out = g1.bm, group.by = c('expl.var', 'algo', 'run'))
+# bm_PlotVarImpBoxplot(bm.out = g1.bm, group.by = c('algo', 'expl.var', 'run'))
+
+# # Represent response curves
+# bm_PlotResponseCurves(bm.out = g1.bm, 
+#                       models.chosen = get_built_models(g1.bm)[c(1:3)],
+#                       fixed.var = 'median')
+
+# # Plot projection
+# plot(g1.bmp)
 
 
-get_calib_lines(g1.bm) %>% plot(g1.bmdata, calib.lines = .)
 
-
-
-
-
-# Get evaluation scores & variables importance
-get_evaluations(g1.bm)
-get_variables_importance(g1.bm) %>% 
-  as_tibble() %>%
-  filter(run != "allRun") %>%
-  ggplot(aes(x = expl.var, y = var.imp, col = run)) +
-  facet_wrap(~ expl.var, scales = "free_x") +
-  geom_boxplot(outlier.colour = NA) +
-  geom_point(position = position_jitterdodge()) +
-  theme_classic() +
-  theme(axis.text.x = element_blank())
-
-# Represent evaluation scores & variables importance
-bm_PlotEvalMean(bm.out = g1.bm)
-
-bm_PlotEvalBoxplot(bm.out = g1.bm, group.by = c('algo', 'algo'))
-bm_PlotEvalBoxplot(bm.out = g1.bm, group.by = c('algo', 'run'))
-bm_PlotVarImpBoxplot(bm.out = g1.bm, group.by = c('expl.var', 'algo', 'algo'))
-bm_PlotVarImpBoxplot(bm.out = g1.bm, group.by = c('expl.var', 'algo', 'run'))
-bm_PlotVarImpBoxplot(bm.out = g1.bm, group.by = c('algo', 'expl.var', 'run'))
-
-# Represent response curves
-bm_PlotResponseCurves(bm.out = g1.bm, 
-                      models.chosen = get_built_models(g1.bm)[c(1:3)],
-                      fixed.var = 'median')
-
-
-
-g1.bmp <- BIOMOD_Projection(bm.mod = g1.bm,
-                                  proj.name = 'Current',
-                                  new.env = bioclim6180,
-                                  models.chosen = 'all',
-                                  metric.binary = 'all',
-                                  metric.filter = 'all',
-                                  build.clamping.mask = TRUE)
-g1.bmp
-plot(g1.bmp)
 
